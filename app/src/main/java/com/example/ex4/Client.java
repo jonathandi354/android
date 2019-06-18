23a89285f0366f41952f39b07850470648900af1
jonathandi1 <jonathan.diamant1@gmail.con>
2019-06-18 18:49:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ex4/Client.java
@@ -0,0 +1,50 @@
+package com.example.ex4;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+    private String ip;
+    private int port;
+    private OutputStream stream;
+    private Socket socket;
+
+    public Client(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public void Connect() {
+        try {
+            InetAddress server =InetAddress.getByName(ip);
+            socket = new Socket(server, port);
+            try {
+                stream =  socket.getOutputStream();
+
+            } catch (Exception e) {
+
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void send(String msg) {
+        try {
+            stream.write(msg.getBytes());
+            stream.flush();
+
+        } catch (Exception e) {
+
+        }
+    }
+    public void Disconnect() {
+        try {
+            socket.close();
+        } catch (Exception e) {
+
+        }
+    }
+}
--- app/src/main/java/com/example/ex4/JoystickView.java
+++ app/src/main/java/com/example/ex4/JoystickView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.RectF;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -14,21 +15,31 @@ import androidx.core.view.MotionEventCompat;
 public class JoystickView extends View {
     private Paint color_in;
     private Paint color_out;
-    private int r_in;
-    private int r_out;
+    private int r_in = 100;
+    private int r_out = 500;
     private int x_in;
     private int y_in;
     private int x_out;
     private int y_out;
     private boolean move = false;
     private boolean pressed = false;
-
+    private int x_cur;
+    private int y_cur;
     public JoystickView(Context context) {
         super(context);
         color_in = new Paint(Paint.ANTI_ALIAS_FLAG);
         color_out = new Paint(Paint.ANTI_ALIAS_FLAG);
         color_in.setColor(Color.BLUE);
         color_out.setColor(Color.YELLOW);
+        int mWidth= this.getResources().getDisplayMetrics().widthPixels;
+        int mHeight= this.getResources().getDisplayMetrics().heightPixels;
+        x_in = mWidth / 2;
+        y_in = mHeight / 2;
+        x_out = mWidth / 2;
+        y_out = mHeight / 2;
+        x_cur = x_in;
+        y_cur = y_in;
+
     }
     @Override
     ///////////////////////
@@ -43,7 +54,7 @@ public class JoystickView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         RectF out = new RectF(x_out - r_out, y_out - r_out, x_out+r_out, y_out+r_out);
-        RectF in  = new RectF(x_in - r_in, y_in - r_in, x_in+r_in, y_in+r_in);
+        RectF in  = new RectF(x_cur - r_in, y_cur - r_in, x_cur+r_in, y_cur+r_in);
         canvas.drawOval(out, color_out);
         canvas.drawOval(in, color_in);
 
@@ -70,20 +81,36 @@ public class JoystickView extends View {
                 float y = event.getY();
 
                 if (inside_bounderies(x,y)) {
-                    x_in = (int)x;
-                    y_in = (int)y;
+                    x_cur = (int)x;
+                    y_cur = (int)y;
                     //send to someone....
+                    invalidate();
                 }
+                break;
 
             }
+            case MotionEvent.ACTION_UP: {
+                x_cur = x_in;
+                y_cur = y_in;
+                invalidate();
+
+            }
+            case MotionEvent.ACTION_CANCEL: {
+                pressed = false;
+                break;
+            }
+
         }
+        return true;
     }
+
+
 
     private float dis(float x1, float y1, float x2, float y2) {
         return (float)Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
     }
     private boolean inside_bounderies(float x,  float y) {
-        if (dis(x,y,x_out, y_out) < r_out) {
+        if (dis(x,y,x_out, y_out) < r_out - r_in) {
             return true;
         }
         return false;
